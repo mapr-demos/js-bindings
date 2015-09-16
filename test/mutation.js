b0d6e4cca4e788278ddbf7465e436be4f568a9bb
jblazecybervision <jblaze.cybervision@gmail.com>
2015-09-16 20:25:30 +0300

    Added human friendly assertion.

--- test/mutation.js
+++ test/mutation.js
@@ -9,23 +9,33 @@ describe('Mutation', function () {
     [
       {'field': {'$set': 'value'}},
       {'age': {'$set': 34}},
+      {'age': {'$set': true}},
+      {'age': {'$set': null}},
+      {'age': {'$set': {}}},
+      {'age': {'$set': [{a: 1}]}},
+      {'age': {'$set': new Date()}},
       {'age': {'$set': 34}, 'country': {'$set': 'USA'}},
       {'interests': {'$set': ['x', 'y']}},
       {'address': {'$set': {'t': 'home'}}},
       {'field': {'$setOrReplace': 'value'}},
       {'age': {'$setOrReplace': 34}},
+      {'age': {'$setOrReplace': true}},
+      {'age': {'$setOrReplace': {}}},
+      {'age': {'$setOrReplace': new Date()}},
+      {'age': {'$setOrReplace': [{a: 1}]}},
+      {'age': {'$setOrReplace': null}},
       {'age': {'$setOrReplace': 34}, 'country': {'$setOrReplace': 'USA'}},
       {'field': {'$append': 'value'}},
       {'name': {'$append': 'Doe'}},
+      {'name': {'$append': [1,2,3]}},
       {'field': {'$setOrReplace': 'value'}},
       {'age': {'$setOrReplace': 34}},
       {'age': {'$setOrReplace': 34}, 'country': {'$setOrReplace': 'USA'}},
-      {'field': {'$inc': 'value'}},
       {'age': {'$inc': 1}},
       {'age': {'$delete': true}}
     ].forEach(function (json) {
         it(JSON.stringify(json), function () {
-          var m = new mutation(this.json);
+          var m = new mutation(json);
           assert.ok(java.instanceOf(m.jMutation, 'com.mapr.db.Mutation'));
           m.build();
           assert.ok(java.instanceOf(m.jMutation, 'com.mapr.db.Mutation'));
@@ -35,7 +45,7 @@ describe('Mutation', function () {
 
   describe('manual input', function () {
 
-    var m = new mutation();
+    var m = new mutation({});
     m.set('f1', 'v1').set('f2', [1, 2]).
       setOrReplace('f3', 3).setOrReplace('f4', '4').
       append('f6', '6').append('f10', [1, 2, 3]).
@@ -44,6 +54,40 @@ describe('Mutation', function () {
     assert.ok(java.instanceOf(m.jMutation, 'com.mapr.db.Mutation'));
     m.build();
     assert.ok(java.instanceOf(m.jMutation, 'com.mapr.db.Mutation'));
+  });
+
+  describe('errors thrown', function () {
+
+    it('append not a string or array', function () {
+      assert.throw(function () {
+        new mutation({'field': {$append: 1}});
+      });
+    });
+
+    it('inc not a number', function () {
+      assert.throw(function () {
+        new mutation({'field': {$inc: ''}});
+      });
+    });
+
+    it('bad mutation key', function () {
+      assert.throw(function () {
+        new mutation({'field': 'value'});
+      });
+    });
+
+    it('bad constructor arguments', function () {
+      assert.throw(function () {
+        new mutation('value');
+      });
+    });
+
+    it('empty constructor arguments', function () {
+      assert.throw(function () {
+        new mutation();
+      });
+    });
+
   });
 
 });
